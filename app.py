4695eff172e78cc1c5ba1ff8ffce4ee4ebeb01b7
amandahp <amandahoffmann96@gmail.com>
2020-10-17 14:20:58 -0300

    new

--- app.py
+++ app.py
@@ -2,17 +2,18 @@
 # coding: utf-8
 
 #Importação do microFramework Flask
-from flask import Flask
+from flask import Flask, render_template
 
 #Criação da aplicação web, passando como paramêtro o nome do projeto em questão
 app = Flask('projeto1')
 
 #Decorator que define a rota dessa função
-@app.route("/")
+@app.route("/condicional/")
 #Criação de uma função simples em Python
-def hello_world():
+def condicional():
+    #meu_nome = "amanda"
     #resultado que será apresentado na página
-    return "Hello World! This is my first WebApp", 200
+    return render_template("modelo_condicional.html"), 200 #nome=meu_nome), 200
 
 #comando para rodar aplicação 
 app.run()
